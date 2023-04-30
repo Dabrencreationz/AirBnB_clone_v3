48af1becc9329543a44f595feb55b935af60d3db
2022phyro <phyrokelstein2@gmail.com>
2023-04-30 14:21:55 +0000

    Fixed syntax errors

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -19,14 +19,14 @@ def get_all_place_reviews(place_id):
     if not val:
         abort(404)
     if request.method == 'GET':
-        return jsonify([x.to_dict() for x in val.reviews)
+        return jsonify([x.to_dict() for x in val.reviews])
     elif request.method == 'POST':
         data = request.get_json()
         if data is None:
             return make_response(jsonify("Not a JSON"), 400)
         if 'user_id' not in data:
             return make_response(jsonify("Missing user_id"), 400)
-        user = storage.get(cls=User, id=data['user_id']
+        user = storage.get(cls=User, id=data['user_id'])
         if user is None:
             abort(404)
         if 'text' not in data:
@@ -37,7 +37,7 @@ def get_all_place_reviews(place_id):
         return make_response(jsonify(rev.to_dict()), 201)
 
 
-@app_views.route('/reviews/review_id>', strict_slashes=False,
+@app_views.route('/reviews/<review_id>', strict_slashes=False,
                  methods=['GET', 'PUT', 'DELETE'])
 def work_with_review_id(review_id):
     """This function performs various tasks according to the
