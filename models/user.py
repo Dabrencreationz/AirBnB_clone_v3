adec3c65421db73585570037379d346738e836b8
2022phyro <phyrokelstein2@gmail.com>
2023-05-02 23:04:46 +0000

    attempting to debug no 14

--- models/user.py
+++ models/user.py
@@ -14,7 +14,7 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        _password = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
@@ -29,15 +29,19 @@ class User(BaseModel, Base):
     def password(self):
         """Getter attribute"""
         if models.storage_t == 'db':
-            return self.__dict__.get('_password', None)
-        return self.__dict__.get('_password', "")
+            return self.__dict__.get('password', None)
+        return self.__dict__.get('password', "")
 
     @password.setter
     def password(self, password):
         """Hashes the password"""
         p_w = md5(bytes(password.encode('utf-8'))).hexdigest()
-        self.__dict__['_password'] = p_w
+        self.__dict__['password'] = p_w
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if kwargs:
+            p_word = kwargs.pop('password', None)
+            if p_word:
+                self.password = p_word
         super().__init__(*args, **kwargs)
